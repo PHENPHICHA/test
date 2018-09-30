e643bda7f8d838e8c6a14f3a9078744c2477abe2
PHENPHICHA <5910110236@psu.ac.th>
2018-10-01 02:01:02 +0700

    CHECK POINT #5

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
-import Weather from './components/Weather'
 import { StyleSheet, Text, View } from 'react-native';
- export default class App extends React.Component {
-  
+import WeatherScreen from './components/WeatherScreen'
+import {createStackNavigator} from 'react-navigation'
+import ZipCodeScreen from './components/ZipCodeScreen';
+
+const RootStack = createStackNavigator({
+  Weather: WeatherScreen,
+  ZipCode: ZipCodeScreen,
+},{
+  initialRouteName: 'Weather',
+})
+
+export default class App extends React.Component {
+
   render() {
     return (
-      <Weather zipCode="90110"/>
+      <RootStack/>
     );
   }
 }
- 
