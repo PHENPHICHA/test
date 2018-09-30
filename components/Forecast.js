52ec4a978b8314cacb706ef279f24757590f86a2
PHENPHICHA <5910110236@psu.ac.th>
2018-09-30 21:25:03 +0700

    1

--- components/Forecast.js
+++ components/Forecast.js
@@ -21,5 +21,5 @@ const styles = StyleSheet.create({
     container: {
         paddingTop: 25 
     },
-    backdrop: {width: '100%', height: '100%'},
+    backdrop: {width: '100%',  height: '100%'},
   }); 
